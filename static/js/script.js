42e07f5135233642448b2b10a7dae52c01ca8b96
Dave Wheeler <dwheelerau@gamil.com>
2018-07-14 12:18:00 +1000

    x axis is owrking, just need to fix y names

--- static/js/script.js
+++ static/js/script.js
@@ -7,160 +7,156 @@
 // if you uncomment the above function
 // setup svg margin conventions
 
-var choice = ["Australia", "NZ"];
+var choice = ["Australia", "New Zealand"];
 var colors = {"Australia": "#ffe200",
                 "NZ": "black"};
 var currentChoice = choice[0];
 // document.getElementById("h2").innerHTML = " World cup leading try scorers";
 d3.selectAll("h2").text(currentChoice + " World Cup leading try scorers");
-var datajson = {"Australia": [{"name": "apple",
-    "score": 1},
-    {"name": "ban",
-        "score": 2},
-    {"name": "can",
-        "score": 3},
-    {"name": "dan",
-        "score": 4}],
-    "NZ": [{"name": "zam",
-        "score": 4},
-    {"name": "xam",
-        "score": 6},
-    {"name": "pam",
-        "score": 2}],
-};
+var dataJson;
 
-var margin = {
-    top: 15,
-    right: 25,
-    bottom: 15,
-    left: 150};
-
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-
-var svg = d3.select("#graphic")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var x = d3.scale.linear()
-    .range([0, width])
-    .domain([0, d3.max(datajson.Australia, function(d) {
-        return d.score;
-    })]);
-
-var y = d3.scale.ordinal()
-    .rangeRoundBands([height, 0], 0.1)
-    .domain(datajson.Australia.map(function(d) {
-        return d.name;
-    }));
-
-// draw the axes
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .tickSize(0) // no ticks
-    .orient("left"); // barh
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .tickSize(0) // no ticks
-    .orient("bottom");
-var gy = svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
-var gx = svg.append("g")
-    .attr("class", "x axis")
-    .call(xAxis);
-// append the data to the bars
-var bars = svg.selectAll(".bar")
-    .data(datajson.Australia)
-    .enter()
-    .append("g");
-// add the rectangles
-bars.append("rect")
-    .attr("class", "bar")
-    .attr("y", function(d) {
-        return y(d.name);
-    })
-    .attr("height", y.rangeBand())
-    .attr("x", 0)
-    .attr("width", function(d) {
-        return x(d.score);
-    })
-    .attr("fill", colors[currentChoice]);
-// add labels to right of each bar
-bars.append("text")
-    .attr("class", "label")
-    // pos of lab half down bar
-    .attr("y", function(d) {
-        return y(d.name) + y.rangeBand() / 2 + 4;
-    })
-    // pos 3 pix to the right of the bar
-    .attr("x", function(d) {
-        return x(d.score) + 3;
-    })
-    .text(function(d) {
-        return d.score;
-    });
-
-// onclick function to update data
-bars.on('click', function() {
-    if (currentChoice == choice[0]) {
-        currentChoice = choice[1];
-    } else {
-        currentChoice = choice[0];
+d3.json("static/data/data2.json", function(error, data) {
+    if (error) {
+        throw error;
     }
-    d3.selectAll("h2").text(currentChoice + " World Cup leading try scorers");
-    // update axes
-    y.domain(datajson[currentChoice].map(function(d) {
-        return d.name;
-    }));
-    x.domain([0, d3.max(datajson[currentChoice], function(d) {
-        return d.score;
-    })]);
-    // call the axis to update
-    gy.call(yAxis);
-    gx.call(xAxis);
-    // update the data, need enter because size has changed
-    svg.selectAll(".bar")
-        .data(datajson[currentChoice])
+    dataJson = data; 
+    drawBox();
+});
+
+function drawBox() {
+    var margin = {
+        top: 15,
+        right: 25,
+        bottom: 15,
+        left: 150};
+
+    var width = 960 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+
+    var svg = d3.select("#graphic")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([0, d3.max(dataJson.Australia, function(d) {
+            return d.try;
+        })]);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([height, 0], 0.1)
+        .domain(dataJson.Australia.map(function(d) {
+            console.log(d.name);
+            return d.name.slice(0,10);
+        }));
+
+    // draw the axes
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickSize(0) // no ticks
+        .orient("left"); // barh
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(0) // no ticks
+        .orient("bottom");
+    var gy = svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+    var gx = svg.append("g")
+        .attr("class", "x axis")
+        .call(xAxis);
+    // append the data to the bars
+    var bars = svg.selectAll(".bar")
+        .data(dataJson.Australia)
         .enter()
         .append("g");
-    // now redarw the rectangles using new data
-    bars.selectAll('rect')
-        .transition()
+    // add the rectangles
+    bars.append("rect")
+        .attr("class", "bar")
         .attr("y", function(d) {
-            // 'y' here is the y scale
             return y(d.name);
         })
         .attr("height", y.rangeBand())
         .attr("x", 0)
         .attr("width", function(d) {
-        return x(d.score);
+            return x(d.score);
         })
-        .each("end", function() { // <-- Executes at end of transition
-            d3.select(this)
-            .attr("fill", colors[currentChoice]);
-        });
-    // add the new data to the labels using the class name
-    svg.selectAll(".label")
-       .data(datajson[currentChoice])
-       .enter();
-    // use this data to update text and position
-    bars.selectAll("text")
-        .transition()
+        .attr("fill", colors[currentChoice]);
+    // add labels to right of each bar
+    bars.append("text")
+        .attr("class", "label")
         // pos of lab half down bar
         .attr("y", function(d) {
             return y(d.name) + y.rangeBand() / 2 + 4;
         })
         // pos 3 pix to the right of the bar
         .attr("x", function(d) {
-            console.log(d.score);
             return x(d.score) + 3;
         })
         .text(function(d) {
             return d.score;
         });
-});
-// });
+
+    // onclick function to update data
+    bars.on('click', function() {
+        if (currentChoice == choice[0]) {
+            currentChoice = choice[1];
+        } else {
+            currentChoice = choice[0];
+        }
+        d3.selectAll("h2").text(currentChoice + " World Cup leading try scorers");
+        // update axes
+        y.domain(dataJson[currentChoice].map(function(d) {
+            return d.name;
+        }));
+        x.domain([0, d3.max(dataJson[currentChoice], function(d) {
+            return d.score;
+        })]);
+        // call the axis to update
+        gy.call(yAxis);
+        gx.call(xAxis);
+        // update the data, need enter because size has changed
+        svg.selectAll(".bar")
+            .data(dataJson[currentChoice])
+            .enter()
+            .append("g");
+        // now redarw the rectangles using new data
+        bars.selectAll('rect')
+            .transition()
+            .attr("y", function(d) {
+                // 'y' here is the y scale
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function(d) {
+            return x(d.score);
+            })
+            .each("end", function() { // <-- Executes at end of transition
+                d3.select(this)
+                .attr("fill", colors[currentChoice]);
+            });
+        // add the new data to the labels using the class name
+        svg.selectAll(".label")
+        .data(dataJson[currentChoice])
+        .enter();
+        // use this data to update text and position
+        bars.selectAll("text")
+            .transition()
+            // pos of lab half down bar
+            .attr("y", function(d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            // pos 3 pix to the right of the bar
+            .attr("x", function(d) {
+                console.log(d.score);
+                return x(d.score) + 3;
+            })
+            .text(function(d) {
+                return d.score;
+            });
+    });
+}
